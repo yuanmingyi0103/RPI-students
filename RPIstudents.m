d8f05cd624a230b082cd15cc3070491696b47911
Cindy Yuan <ymy.yuan@gmail.com>
2015-05-28 15:22:41 -0400

    changed all the answers into numbers

--- RPIstudents.m
+++ RPIstudents.m
@@ -3,4 +3,6 @@ clear;
 close all;
 
 %% Load in the data
-[num,txt,raw] = xlsread('data.xlsx');
+[num,txt,raw] = xlsread('NoCourses.xlsx');
+raw(:,2:36)
+%% Change ABCD into numbers for first 35 survey questions
