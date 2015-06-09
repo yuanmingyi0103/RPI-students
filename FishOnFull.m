f6e11335a31d67c6addfc5f27e4f87a42b55a5de
allena95 <allen.alexander.d17@gmail.com>
2015-06-09 10:45:53 -0400

    removed some unnecessary columns

--- FishOnFull.m
+++ FishOnFull.m
@@ -1,3 +1,20 @@
 %% 
 
-[num,txt,raw] = xlsread('NoCoursesWithGPA.xlsx');
+[num,txt,raw] = xlsread('DataWoutCat.xlsx');
+
+num = num(~any(isnan(num),2),:); %remove students with missing data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
