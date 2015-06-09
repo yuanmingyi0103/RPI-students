4f6eb8369e4fcf87726497a6a612a550775042b0
allena95 <allen.alexander.d17@gmail.com>
2015-06-09 10:30:51 -0400

    Data without categorical variables

--- RPIStudentsCovariance.m
+++ RPIStudentsCovariance.m
@@ -5,7 +5,7 @@ close all;
 %% Load in the data
 [num,txt,raw] = xlsread('NoCoursesWithGPA.xlsx');
 
-data = [num(:,2:41) num(:,44:53) num(:,56)]; % remove survey number
+data = [num(:,2:41) num(:,45:54) num(:,57)]; % remove survey number
 
 data = data(~any(isnan(data),2),:); %remove students with missing data
 
